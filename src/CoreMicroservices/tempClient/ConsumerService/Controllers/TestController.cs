ea5d13276b683a82a33637f7f02f086dc97e1a96
shimon <Shimond@any-techs.co.il>
2019-11-17 20:31:16 +0200

    add refit

---
+++ src/CoreMicroservices/tempClient/ConsumerService/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CurrencyApiLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Refit;
+
+namespace ConsumerService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger, IHttpClientFactory clientFactory)
+        {
+            _httpClient = clientFactory.CreateClient("CURRRENCY");
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await RestService.For<ICurrencyAPI>(this._httpClient).GetAll();
+            return Ok(result);
+        }
+    }
+}
---
+++ src/CoreMicroservices/tempClient/ConsumerService/Startup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ConsumerService.Controllers;
+using CurrencyApiLib;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Extensions.Http;
+
+namespace ConsumerService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+
+        static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+        {
+            return HttpPolicyExtensions
+                        .HandleTransientHttpError()
+                        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                        .WaitAndRetryAsync(6, retryAttempt =>
+                                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddHttpClient("CURRRENCY", client =>
+                          client.BaseAddress = new Uri(Configuration["services:currencies"]))
+                .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+                .AddPolicyHandler(GetPolicy());
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
