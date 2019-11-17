68fc36493355c393ea830ea904c880d2ea1ac22f
shimon <Shimond@any-techs.co.il>
2019-11-17 19:18:12 +0200

    refit and configuration

---
+++ src/CoreMicroservices/apis/CurrencyApiLib/ICurrencyAPI.cs
@@ -0,0 +1,16 @@
+﻿using CurrencyApiLib.model;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CurrencyApiLib
+{
+    public interface ICurrencyAPI
+    {
+        [Get("/currencies")]
+        Task<List<CurrencyDTO>> GetAll();
+        [Get("/currencies/{currencyCode}")]
+        Task<CurrencyDTO> GetById(string currencyCode);
+    }
+}
